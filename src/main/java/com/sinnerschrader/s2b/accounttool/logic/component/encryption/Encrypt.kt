5d3613e25f32ab8306828cffeba621373bebf27f
s2ralste <s2ralste@users.noreply.github.com>
2017-05-05 16:27:49 +0200

    moved encryption to single file

---
+++ src/main/java/com/sinnerschrader/s2b/accounttool/logic/component/encryption/Encrypt.kt
@@ -0,0 +1,29 @@
+package com.sinnerschrader.s2b.accounttool.logic.component.encryption;
+
+import org.apache.commons.codec.digest.Crypt
+import org.apache.commons.lang3.RandomStringUtils
+import org.springframework.security.crypto.codec.Hex
+import sun.security.provider.MD4
+import java.io.UnsupportedEncodingException
+
+object Encrypt {
+
+    fun salt(password: String) = salt(password, RandomStringUtils.randomAlphanumeric(16))
+    fun salt(password: String, salt: String): String {
+        if (password.isEmpty() or salt.isEmpty()) {
+            throw IllegalArgumentException("Password and salt can't be null or empty")
+        }
+        return "{CRYPT}" + Crypt.crypt(password, "$6$" + salt)
+    }
+
+    fun samba(password: String): String {
+        if (password.isEmpty()) {
+            throw IllegalArgumentException("Password can't be null or empty")
+        }
+        try {
+            return String(Hex.encode(MD4.getInstance().digest(password.toByteArray(charset("UTF-16LE"))))).toUpperCase()
+        } catch (uee: UnsupportedEncodingException) {
+            throw RuntimeException("Could not generate samba password hash", uee)
+        }
+    }
+}
