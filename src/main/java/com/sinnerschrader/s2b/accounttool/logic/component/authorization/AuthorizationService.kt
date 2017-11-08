04aa1eb85eda78010a77e6e86e4a8f281484a08a
s2ralste <s2ralste@users.noreply.github.com>
2017-11-08 12:55:08 +0100

    removed exception

---
+++ src/main/java/com/sinnerschrader/s2b/accounttool/logic/component/authorization/AuthorizationService.kt
@@ -0,0 +1,63 @@
+package com.sinnerschrader.s2b.accounttool.logic.component.authorization
+
+import com.sinnerschrader.s2b.accounttool.config.authentication.LdapUserDetails
+import com.sinnerschrader.s2b.accounttool.config.ldap.LdapConfiguration
+import org.apache.commons.lang3.StringUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.stereotype.Service
+
+
+@Service
+class AuthorizationService {
+
+    @Autowired
+    private val ldapConfiguration: LdapConfiguration? = null
+
+    private fun isMemberOf(authorities: Collection<GrantedAuthority>, group: String?): Boolean {
+        for (ga in authorities)
+            if (StringUtils.equals(ga.authority, group))
+                return true
+        return false
+    }
+
+    fun isAdmin(user: LdapUserDetails) = isMemberOf(user.authorities, ldapConfiguration!!.permissions.ldapAdminGroup)
+
+    fun isUserAdministration(user: LdapUserDetails): Boolean {
+        for (userAdminGroup in ldapConfiguration!!.permissions.userAdminGroups!!)
+            if (isMemberOf(user.authorities, userAdminGroup))
+                return true
+        return false
+    }
+
+    fun isGroupAdmin(user: LdapUserDetails, groupCn: String): Boolean {
+        val prefixSuffix = "-" //yepp, a suffix on a prefix.
+        val adminPrefix = ldapConfiguration!!.groupPrefixes.admin + prefixSuffix
+        val technicalPrefix = ldapConfiguration.groupPrefixes.technical + prefixSuffix
+        val teamPrefix = ldapConfiguration.groupPrefixes.team + prefixSuffix
+        if (StringUtils.startsWith(groupCn, adminPrefix) || StringUtils.startsWith(groupCn, technicalPrefix)) {
+            return isMemberOf(user.authorities, groupCn)
+        }
+        return if (StringUtils.startsWith(groupCn, teamPrefix)) {
+            isMemberOf(user.authorities, StringUtils.replace(groupCn, teamPrefix, adminPrefix))
+        } else false
+    }
+
+    @Throws(AccessDeniedException::class)
+    fun ensureUserAdministration(user: LdapUserDetails) {
+        if (isAdmin(user) || isUserAdministration(user)) {
+            return
+        }
+        throw AccessDeniedException("User is not member of required groups [user=$user]")
+    }
+
+    @Throws(AccessDeniedException::class)
+    fun ensureGroupAdministration(user: LdapUserDetails, group: String) {
+        if (isAdmin(user) || isGroupAdmin(user, group)) {
+            return
+        }
+        throw AccessDeniedException("User is not member of required group [user=$user]")
+    }
+
+}
