70980d0fbee63ae8de2afae69c9dfd63576e95e5
s2ralste <s2ralste@users.noreply.github.com>
2018-07-17 11:48:28 +0200

    ldapservice refactoring

--- src/main/kotlin/com/sinnerschrader/s2b/accounttool/logic/component/ldap/LdapBusinessService.kt
+++ src/main/kotlin/com/sinnerschrader/s2b/accounttool/logic/component/ldap/LdapBusinessService.kt
@@ -1,36 +1,18 @@
 package com.sinnerschrader.s2b.accounttool.logic.component.ldap
 
-import com.google.common.cache.Cache
-import com.google.common.cache.CacheBuilder
 import com.sinnerschrader.s2b.accounttool.config.ldap.LdapConfiguration
 import com.sinnerschrader.s2b.accounttool.config.ldap.LdapManagementConfiguration
-import com.sinnerschrader.s2b.accounttool.logic.DateTimeHelper
 import com.sinnerschrader.s2b.accounttool.logic.component.mail.AccountExpirationMail
 import com.sinnerschrader.s2b.accounttool.logic.component.mail.MailService
 import com.sinnerschrader.s2b.accounttool.logic.component.mail.UnmaintainedUsersMail
 import com.sinnerschrader.s2b.accounttool.logic.entity.User
-import com.unboundid.ldap.sdk.LDAPException
-import org.apache.commons.lang3.ObjectUtils.defaultIfNull
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.InitializingBean
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Service
-import org.springframework.util.CollectionUtils
-import java.security.GeneralSecurityException
 import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.temporal.ChronoUnit
-import java.util.*
-import java.util.Collections.sort
-import java.util.concurrent.TimeUnit
-
 
 @Service
-class LdapBusinessService : InitializingBean {
-
-    private var userCache: Cache<String, List<User>>? = null
-
+class LdapBusinessService {
     @Autowired
     private lateinit var ldapService: LdapService
 
@@ -43,102 +25,58 @@ class LdapBusinessService : InitializingBean {
     @Autowired
     private lateinit var mailService: MailService
 
-    val unmaintainedMailUsers: List<User>
-        get() = getUsersByCacheType(ACTIVE_MAIL_ON_INACTIVE_USER)
+    fun leavingUsers(): List<User> {
+        val today = LocalDate.now()
+        val nextWeeks = managementConfiguration.leavingUsersInCW
+        val nearFuture = today.plusWeeks(nextWeeks.toLong())
 
-    val unmaintainedExternals: List<User>
-        get() = getUsersByCacheType(EXITED_ACTIVE_USERS)
+        return ldapConfiguration.createConnection().use { connection ->
+            connection.bind(managementConfiguration.user.bindDN, managementConfiguration.user.password)
+            ldapService.getUsers(connection).filter {
+                val exitDate = it.szzExitDate
+                (exitDate != null && it.szzStatus == User.State.active &&
+                    !exitDate.isBefore(today) && exitDate.isBefore(nearFuture))
 
-    val leavingUsers: List<User>
-        get() = getUsersByCacheType(NEAR_FUTURE_EXITING)
-
-    @Throws(Exception::class)
-    override fun afterPropertiesSet() {
-        userCache = CacheBuilder.newBuilder()
-                .expireAfterWrite(24, TimeUnit.HOURS)
-                .build()
+            }
+        }
     }
 
-    @Scheduled(cron = "\${ldap-management.jobs.updateUnmaintained.cronExpr}")
-    protected fun updateUnmaintained() {
-        if (!managementConfiguration.jobs.updateUnmaintained.isActive || !managementConfiguration.jobs.isActive) {
-            return
-        }
-
-        log.debug("Updating the informations about unmaintained accounts")
-        val startTime = LocalDateTime.now()
-        val nextWeeks = managementConfiguration.leavingUsersInCW
-        val exitedActiveUsers = LinkedList<User>()
-        val futureExitingUser = LinkedList<User>()
-        val activeMailAccounts = LinkedList<User>()
-        try {
-            ldapConfiguration.createConnection().use { connection ->
-                connection.bind(managementConfiguration.user.bindDN,
-                        managementConfiguration.user.password)
-
-                val userCount = ldapService.getUserCount(connection)
-                val blockSize = 250
-                val today = LocalDate.now()
-                val nearFuture = today.plusWeeks(nextWeeks.toLong())
-                var i = 0
-                while (i < userCount) {
-                    val users = ldapService.getUsers(connection, i, blockSize)
-                    if (CollectionUtils.isEmpty(users)) {
-                        i = i + blockSize
-                        continue
-                    }
-
-                    for (user in users) {
-                        val exitDate = user.szzExitDate
-                        if (exitDate != null && user.szzStatus === User.State.active) {
-                            if (exitDate.isBefore(today)) {
-                                exitedActiveUsers.add(user)
-                            } else if (exitDate.isBefore(nearFuture)) {
-                                futureExitingUser.add(user)
-                            }
-                        }
-                        if (user.szzStatus === User.State.inactive && user.szzMailStatus === User.State.active) {
-                            activeMailAccounts.add(user)
-                        }
-
-                    }
-                    i = i + blockSize
-                }
-                log.debug("Found {} user who are exited but have active accounts", exitedActiveUsers.size)
-                log.debug("Found {} user who are leaving in the next {} weeks", futureExitingUser.size, nextWeeks)
-                log.debug("Found {} active mail addresses on inactive accounts", activeMailAccounts.size)
-
-                sort(exitedActiveUsers)
-                sort(futureExitingUser)
-                sort(activeMailAccounts)
-
-                userCache!!.put(EXITED_ACTIVE_USERS, exitedActiveUsers)
-                userCache!!.put(NEAR_FUTURE_EXITING, futureExitingUser)
-                userCache!!.put(ACTIVE_MAIL_ON_INACTIVE_USER, activeMailAccounts)
-
-                log.info("Updated the informations about unmaintained accounts in {}",
-                        DateTimeHelper.getDurationString(startTime, LocalDateTime.now(), ChronoUnit.MILLIS))
+    fun unmaintainedExternals(): List<User> {
+        val today = LocalDate.now()
+        return ldapConfiguration.createConnection().use { connection ->
+            connection.bind(managementConfiguration.user.bindDN, managementConfiguration.user.password)
+            ldapService.getUsers(connection).filter {
+                val exitDate = it.szzExitDate
+                (it.szzStatus == User.State.active && exitDate?.isBefore(today) ?: false)
             }
-        } catch (e: LDAPException) {
-            log.error("Could not update unmaintained users", e)
-        } catch (e: GeneralSecurityException) {
-            log.error("Could not update unmaintained users", e)
         }
+    }
 
+    fun unmaintainedMailUsers(): List<User> {
+        return ldapConfiguration.createConnection().use { connection ->
+            connection.bind(managementConfiguration.user.bindDN, managementConfiguration.user.password)
+            ldapService.getUsers(connection).filter {
+                (it.szzStatus === User.State.inactive && it.szzMailStatus === User.State.active)
+            }
+        }
     }
 
     @Scheduled(cron = "\${ldap-management.jobs.notifyAboutUnmaintained.cronExpr}")
     protected fun notifyAboutUnmaintained() {
-        if (!managementConfiguration.jobs.notifyAboutUnmaintained.isActive || !managementConfiguration.jobs.isActive) {
-            return
+        with(managementConfiguration.jobs){
+            if(!notifyAboutUnmaintained.isActive || !isActive) return
         }
 
-        if (userCache!!.size() > 0) {
+        val leavingUsers = leavingUsers()
+        val unmaintainedMailUsers = unmaintainedMailUsers()
+        val unmaintainedUsers = unmaintainedExternals()
+
+        if (leavingUsers.size + unmaintainedMailUsers.size + unmaintainedUsers.size > 0) {
             val to = managementConfiguration.notifyReceipients.toTypedArray()
             mailService.sendMail(to, UnmaintainedUsersMail(
-                    leavingUsers = getUsersByCacheType(NEAR_FUTURE_EXITING),
-                    unmaintainedMailUsers = getUsersByCacheType(ACTIVE_MAIL_ON_INACTIVE_USER),
-                    unmaintainedUsers = getUsersByCacheType(EXITED_ACTIVE_USERS),
+                    leavingUsers = leavingUsers,
+                    unmaintainedMailUsers = unmaintainedMailUsers,
+                    unmaintainedUsers = unmaintainedUsers,
                     publicDomain = mailService.publicDomain))
         }
     }
@@ -146,23 +84,11 @@ class LdapBusinessService : InitializingBean {
     @Scheduled(cron = "\${ldap-management.jobs.notifyAboutExpiration.cronExpr}")
     fun notifyAboutExpiration() {
         val threshold = LocalDate.now().plusWeeks(2)
-        val expiringAccounts = leavingUsers.filter {
+        val expiringAccounts = leavingUsers().filter {
             threshold.isAfter(it.szzExitDate!!)
         }
         expiringAccounts.forEach {
             mailService.sendMail(listOf(it), AccountExpirationMail(it))
         }
-    }
-
-    private fun getUsersByCacheType(type: String): List<User> {
-        return defaultIfNull<List<User>>(userCache!!.getIfPresent(type), emptyList())
-    }
-
-
-    companion object {
-        private val log = LoggerFactory.getLogger(LdapBusinessService::class.java)
-        private const val NEAR_FUTURE_EXITING = "leavingUsers"
-        private const val EXITED_ACTIVE_USERS = "unmaintainedUsers"
-        private const val ACTIVE_MAIL_ON_INACTIVE_USER = "unmaintainedMailUsers"
     }
 }
--- src/main/kotlin/com/sinnerschrader/s2b/accounttool/logic/entity/Group.kt
+++ src/main/kotlin/com/sinnerschrader/s2b/accounttool/logic/entity/Group.kt
@@ -1,67 +1,34 @@
 package com.sinnerschrader.s2b.accounttool.logic.entity
 
 import org.apache.commons.lang3.builder.CompareToBuilder
-import org.springframework.util.CollectionUtils.containsAny
-import java.util.Arrays.asList
 
 
 /**
  * Interface for different Implementations of Groups.
  * We currently use mainly posixGroups in LDAP, but will use also groupOfNames or groupOfUniqueNames.
  * This Interface combines the relevant informations of all of them and provides a type information
+ *
+ * @param dn LDAP DN of the Group
+ * @param cn The name of the group
+ * @param description A short description about this Group
+ * @param memberIds Contains the list of UIDs or DNs of the Users which are member of the current group.
+ * @param groupType Indentification of LDAP Group Type (Posix, GroupOfNames, etc).
+ * @param objectClasses  Returns a array of all required ObjectClasses for this Group Instance
  */
 interface Group : Comparable<Group> {
-
-    /** The LDAP DN of the Group */
     val dn: String
-
-    /** The name of the group */
     val cn: String
-
-    /** A short description about this Group */
     val description: String
-
-    /** Contains the list of UIDs or DNs of the Users which are member of the current group. */
     val memberIds: List<String>
-
-    /** Indentification of LDAP Group Type (Posix, GroupOfNames, etc). */
     val groupType: GroupType
-
-    /** Returns a array of all required ObjectClasses for this Group Instance */
     val objectClasses: List<String>
-
-    fun hasMember(uid: String, dn: String) = containsAny(memberIds, asList(uid, dn))
-
     val groupClassification: GroupClassification
 
-    /**
-     * Checks if this group is an admin group for a project or internal team.
-     */
-    val isAdminGroup: Boolean get() = groupClassification == GroupClassification.ADMIN
-
-    /**
-     * Checks if this group is an admin group for a project or internal team.
-     */
-    val isTechnicalGroup: Boolean get() = groupClassification == GroupClassification.TECHNICAL
-
-    /**
-     * Checks if this group is an admin group for a project or internal team.
-     */
-    val isTeamGroup: Boolean get() = groupClassification == GroupClassification.TEAM
-
-    /**
-     * Extracts the Prefix from group cn. This prefixes handles some classification on groups.
-     */
     val groupPrefix: String get() = cn.substringBefore("-", missingDelimiterValue = "")
-
-    /**
-     * Returns the Name of the Group without the prefix of the group. If there are several groups for the same
-     * area/customer this will return the same name.
-     */
     val name: String get() = cn.substringAfter("-")
 
-    override fun compareTo(other: Group): Int =
-        CompareToBuilder().append(name, other.name).append(groupPrefix, other.groupPrefix).build()
+    fun hasMember(uid: String, dn: String) = memberIds.contains(uid) || memberIds.contains(dn)
+    override fun compareTo(other: Group) = CompareToBuilder().append(name, other.name).append(groupPrefix, other.groupPrefix).build()!!
 
     /**
      * Classification of a Group if it is a Client Team Admin Group, Client Team Group, Technical Integration Group or unknown.
@@ -70,27 +37,14 @@ interface Group : Comparable<Group> {
 
     /**
      * The LDAP Type which the Group Instance is representing.
+     *
+     * @property Posix Spec: https://tools.ietf.org/html/rfc2307 [Chapter: 2.2 and 4.]
+     * @property GroupOfNames Spec: https://tools.ietf.org/html/rfc4519#page-22 [Chapter: 3.5]
+     * @property GroupOfUniqueNames Spec: https://tools.ietf.org/html/rfc4519#page-22 [Chapter: 3.6]
      */
-    enum class GroupType private constructor(val objectClass: String, val memberAttritube: String) {
-
-        /**
-         * Spec: https://tools.ietf.org/html/rfc2307
-         * Chapter: 2.2 and 4.
-         */
+    enum class GroupType(val objectClass: String, val memberAttritube: String) {
         Posix("posixGroup", "memberUid"),
-
-        /**
-         * Spec: https://tools.ietf.org/html/rfc4519#page-22
-         * Chapter: 3.5
-         */
         GroupOfNames("groupOfNames", "member"),
-
-        /**
-         * Spec: https://tools.ietf.org/html/rfc4519#page-22
-         * Chapter: 3.6
-         */
         GroupOfUniqueNames("groupOfUniqueNames", "uniqueMember")
-
     }
-
 }
