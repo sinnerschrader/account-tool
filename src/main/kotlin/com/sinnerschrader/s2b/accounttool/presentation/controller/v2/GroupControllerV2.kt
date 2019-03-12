0d2e26f92137b1e9c9ecdd68ebae861bb9fb9604
s2ralste2 <s2ralste2@users.noreply.github.com>
2019-03-12 11:42:51 +0100

    fixed ordering

--- src/main/kotlin/com/sinnerschrader/s2b/accounttool/presentation/controller/v2/GroupControllerV2.kt
+++ src/main/kotlin/com/sinnerschrader/s2b/accounttool/presentation/controller/v2/GroupControllerV2.kt
@@ -1,18 +1,24 @@
 package com.sinnerschrader.s2b.accounttool.presentation.controller.v2
 
 import com.sinnerschrader.s2b.accounttool.logic.component.ldap.v2.LdapServiceV2
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@Api(tags = ["Group"], description = "Provides access to groups")
 @RequestMapping("/v2")
 class GroupControllerV2 {
     @Autowired
     lateinit var ldapServiceV2: LdapServiceV2
 
+    @ApiOperation("Retrieve groups")
     @GetMapping("/group")
-    fun getGroups(@RequestParam(required = false) memberUid: String? = null) = ldapServiceV2.getGroups(memberUid = memberUid)
+    fun getGroups(@ApiParam("filter by memberUid") @RequestParam(required = false) memberUid: String? = null) = ldapServiceV2.getGroups(memberUid = memberUid)
 
+    @ApiOperation("Retrieve group by name")
     @GetMapping("/group/{cn}")
     fun getGroup(@PathVariable cn: String) = ldapServiceV2.getGroups(cn = cn).single()
 }
--- src/main/kotlin/com/sinnerschrader/s2b/accounttool/presentation/controller/v2/UserControllerV2.kt
+++ src/main/kotlin/com/sinnerschrader/s2b/accounttool/presentation/controller/v2/UserControllerV2.kt
@@ -2,6 +2,8 @@ package com.sinnerschrader.s2b.accounttool.presentation.controller.v2
 
 import com.sinnerschrader.s2b.accounttool.logic.component.ldap.v2.LdapServiceV2
 import com.sinnerschrader.s2b.accounttool.logic.entity.User.State
+import io.swagger.annotations.Api
+import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.format.annotation.DateTimeFormat
@@ -12,11 +14,13 @@ import java.lang.IllegalArgumentException
 import java.time.LocalDate
 
 @RestController
+@Api(tags = ["User"], description = "Provides access to users")
 @RequestMapping("/v2")
 class UserControllerV2 {
     @Autowired
     lateinit var ldapServiceV2: LdapServiceV2
 
+    @ApiOperation("Retrieve users")
     @GetMapping("/user")
     fun getUser(@ApiParam("account state")
                 @RequestParam(required = false) state: State?,
@@ -29,13 +33,15 @@ class UserControllerV2 {
                 @ApiParam("earliest exit date")
                 @RequestParam(required = false) @DateTimeFormat(iso = DATE) exitDateStart: LocalDate?,
                 @ApiParam("latest exit date")
-                @RequestParam(required = false) @DateTimeFormat(iso = DATE) exitDateEnd: LocalDate?) =
+                @RequestParam(required = false) @DateTimeFormat(iso = DATE) exitDateEnd: LocalDate?,
+                @ApiParam(allowableValues = "a,b,c") company: String?) =
             ldapServiceV2.getUser(state = state,
                     searchTerm = searchTerm,
                     entryDateRange = LdapServiceV2.DateRange.of(entryDateStart, entryDateEnd),
                     exitDateRange = LdapServiceV2.DateRange.of(exitDateStart, exitDateEnd))
 
 
+    @ApiOperation("Retrieve user by uid")
     @GetMapping("/user/{uid}")
     fun getUser(@PathVariable uid: String) = ldapServiceV2.getUser(uid = uid).single()
 }
