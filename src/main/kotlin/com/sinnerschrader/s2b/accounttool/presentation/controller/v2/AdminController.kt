b0c1d4ef1c10ef3e0fd9ed34056dff87e2af3912
s2-ralste <s2-ralste@users.noreply.github.com>
2020-02-04 14:55:22 +0100

    added maintenance endpoint

--- src/main/kotlin/com/sinnerschrader/s2b/accounttool/presentation/controller/v2/AdminController.kt
+++ src/main/kotlin/com/sinnerschrader/s2b/accounttool/presentation/controller/v2/AdminController.kt
@@ -1,10 +1,15 @@
 package com.sinnerschrader.s2b.accounttool.presentation.controller.v2
 
+import com.sinnerschrader.s2b.accounttool.config.WebConstants
+import com.sinnerschrader.s2b.accounttool.logic.component.ldap.LdapService
 import com.sinnerschrader.s2b.accounttool.logic.component.ldap.v2.LdapServiceV2
 import com.sinnerschrader.s2b.accounttool.logic.component.ldap.v2.LdapServiceV2.UserAttributes.FULL
+import com.sinnerschrader.s2b.accounttool.logic.component.ldap.v2.LdapServiceV2.UserAttributes.INFO
 import com.sinnerschrader.s2b.accounttool.logic.entity.User.State
+import com.sinnerschrader.s2b.accounttool.logic.entity.User.State.inactive
 import com.sinnerschrader.s2b.accounttool.presentation.controller.v2.DynamicAllowedValues.Companion.COMPANIES
 import com.sinnerschrader.s2b.accounttool.presentation.controller.v2.DynamicAllowedValues.Companion.USERTYPE
+import com.unboundid.ldap.sdk.LDAPConnection
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import io.swagger.annotations.ApiParam
@@ -13,6 +18,7 @@ import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.format.annotation.DateTimeFormat.ISO.DATE
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
+import springfox.documentation.annotations.ApiIgnore
 import java.time.LocalDate
 
 @RestController
@@ -22,6 +28,9 @@ import java.time.LocalDate
 class AdminController {
     @Autowired
     lateinit var ldapServiceV2: LdapServiceV2
+
+    @Autowired
+    lateinit var ldapService: LdapService
 
     @ApiOperation("Retrieve users")
     @GetMapping("/admin/user")
@@ -50,4 +59,24 @@ class AdminController {
     @ApiOperation("Retrieve user by uid")
     @GetMapping("/admin/user/{uid}")
     fun getUser(@PathVariable uid: String) = ldapServiceV2.getUser(uid = uid, attributes = FULL).single()
+
+
+    @ApiOperation("Retrieve inactive group members")
+    @GetMapping("/admin/maintenance/group/inactive")
+    fun getMaintenanceGroupInactive() =
+            (ldapServiceV2.getUser(state = inactive, attributes = INFO).map { it.uid }).let { inactive ->
+                ldapServiceV2.getGroups().map {
+                    it.name to it.members.apply { retainAll(inactive) }
+                }.toMap().filterValues { it.isNotEmpty() }
+            }
+
+    @ApiOperation("Remove inactive group members")
+    @DeleteMapping("/admin/maintenance/group/inactive")
+    fun deleteMaintenanceGroupInactive(@ApiIgnore
+                                       @RequestAttribute(name = WebConstants.ATTR_CONNECTION)
+                                       connection: LDAPConnection) =
+            // TODO optimise performance (batch change)
+            ldapServiceV2.getUser(state = inactive, attributes = FULL).forEach {
+                ldapService.clearGroups(connection, it)
+            }
 }
